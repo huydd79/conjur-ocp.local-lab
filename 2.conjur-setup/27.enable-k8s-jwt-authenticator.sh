9d81614e0d69fd6d7657f9c5f95a8084d4242fae
Huy Do <86530496+huydd79@users.noreply.github.com>
2024-05-04 00:55:10 +0700

    Update 27.enable-k8s-jwt-authenticator.sh

--- 2.conjur-setup/27.enable-k8s-jwt-authenticator.sh
+++ 2.conjur-setup/27.enable-k8s-jwt-authenticator.sh
@@ -17,6 +17,17 @@ conjur -d policy load -f ./policies/authn-jwt-k8s.yaml -b root
 
 #Below command is for cert-based auth to init ca cert/key pair for conjur CA. No need in case of using jwt
 #sudo podman exec $node_name chpst -u conjur conjur-plugin-service possum rake authn_k8s:ca_init["conjur/authn-jwt/k8s"]
-sudo podman exec -it $node_name sh -c 'grep -q "authn,authn-jwt/k8s" /opt/conjur/etc/conjur.conf || echo "CONJUR_AUTHENTICATORS=\"authn,authn-jwt/k8s\"\n">>/opt/conjur/etc/conjur.conf'
-sudo podman exec $node_name sv restart conjur
+#Enabling all configured authn methods
+tmp_file="/tmp/$(date +%s)-conjur.yml"
+[ -f "$CONF_FILE" ] && mv $CONF_FILE $CONF_FILE.bk.$(date +%s)
+auth_json=$(curl -sk $CONJUR_URL/info | jq '.authenticators.configured')
+cat << EOF > $tmp_file
+#This file is created by script $0
+authenticators: $auth_json
+EOF
+sudo podman exec -it $node_name sh -c "mv /etc/conjur/config/conjur.yml /etc/conjur/config/conjur.yml.bk.$(date +%s)"
+sudo podman cp $tmp_file $node_name:/etc/conjur/config/conjur.yml
+sudo podman exec conjur evoke configuration apply
+
+curl -sk https://conjur-master.$LAB_DOMAIN/info | jq '.authenticators.configured[]'
 set +x
